46e604de2d44c5f097f19f3d5c86f4fc5e6e7e16
DanialNejad <danialabdollahinejad@gmail.com>
2024-06-09 10:51:36 +0330

    cluster assignment

--- graph_embedding/dmon/train.py
+++ graph_embedding/dmon/train.py
@@ -43,6 +43,7 @@ from graph_embedding.dmon import metrics
 from graph_embedding.dmon import utils
 tf.compat.v1.enable_v2_behavior()
 
+
 FLAGS = flags.FLAGS
 
 flags.DEFINE_string(
@@ -81,132 +82,119 @@ flags.DEFINE_float(
     lower_bound=0)
 
 
-def load_npz(
-    filename
-):
-  """Loads an attributed graph with sparse features from a specified Numpy file.
+def load_npz(filename):
+    """Loads an attributed graph with sparse features from a specified Numpy file.
 
-  Args:
-    filename: A valid file name of a numpy file containing the input data.
+    Args:
+        filename: A valid file name of a numpy file containing the input data.
 
-  Returns:
-    A tuple (graph, features, labels, label_indices) with the sparse adjacency
-    matrix of a graph, sparse feature matrix, dense label array, and dense label
-    index array (indices of nodes that have the labels in the label array).
-  """
-  with np.load(open(filename, 'rb'), allow_pickle=True) as loader:
-    loader = dict(loader)
-    adjacency = scipy.sparse.csr_matrix(
-        (loader['adj_data'], loader['adj_indices'], loader['adj_indptr']),
-        shape=loader['adj_shape'])
+    Returns:
+        A tuple (graph, features) with the sparse adjacency
+        matrix of a graph and sparse feature matrix.
+    """
+    with np.load(open(filename, 'rb'), allow_pickle=True) as loader:
+        loader = dict(loader)
+        adjacency = scipy.sparse.csr_matrix(
+            (loader['adj_data'], loader['adj_indices'], loader['adj_indptr']),
+            shape=loader['adj_shape'])
 
-    features = scipy.sparse.csr_matrix(
-        (loader['feature_data'], loader['feature_indices'],
-         loader['feature_indptr']),
-        shape=loader['feature_shape'])
+        features = scipy.sparse.csr_matrix(
+            (loader['feature_data'], loader['feature_indices'],
+             loader['feature_indptr']),
+            shape=loader['feature_shape'])
 
-    label_indices = loader['label_indices']
-    labels = loader['labels']
-  assert adjacency.shape[0] == features.shape[
-      0], 'Adjacency and feature size must be equal!'
-  assert labels.shape[0] == label_indices.shape[
-      0], 'Labels and label_indices size must be equal!'
-  return adjacency, features, labels, label_indices
+    assert adjacency.shape[0] == features.shape[0], 'Adjacency and feature size must be equal!'
+    return adjacency, features
 
 
-def convert_scipy_sparse_to_sparse_tensor(
-    matrix):
-  """Converts a sparse matrix and converts it to Tensorflow SparseTensor.
+def convert_scipy_sparse_to_sparse_tensor(matrix):
+    """Converts a sparse matrix and converts it to Tensorflow SparseTensor.
 
-  Args:
-    matrix: A scipy sparse matrix.
+    Args:
+        matrix: A scipy sparse matrix.
 
-  Returns:
-    A ternsorflow sparse matrix (rank-2 tensor).
-  """
-  matrix = matrix.tocoo()
-  return tf.sparse.SparseTensor(
-      np.vstack([matrix.row, matrix.col]).T, matrix.data.astype(np.float32),
-      matrix.shape)
+    Returns:
+        A tensorflow sparse matrix (rank-2 tensor).
+    """
+    matrix = matrix.tocoo()
+    return tf.sparse.SparseTensor(
+        np.vstack([matrix.row, matrix.col]).T, matrix.data.astype(np.float32),
+        matrix.shape)
 
 
-def build_dmon(input_features,
-               input_graph,
-               input_adjacency):
-  """Builds a Deep Modularity Network (DMoN) model from the Keras inputs.
+def build_dmon(input_features, input_graph, input_adjacency):
+    """Builds a Deep Modularity Network (DMoN) model from the Keras inputs.
 
-  Args:
-    input_features: A dense [n, d] Keras input for the node features.
-    input_graph: A sparse [n, n] Keras input for the normalized graph.
-    input_adjacency: A sparse [n, n] Keras input for the graph adjacency.
+    Args:
+        input_features: A dense [n, d] Keras input for the node features.
+        input_graph: A sparse [n, n] Keras input for the normalized graph.
+        input_adjacency: A sparse [n, n] Keras input for the graph adjacency.
 
-  Returns:
-    Built Keras DMoN model.
-  """
-  output = input_features
-  for n_channels in FLAGS.architecture:
-    output = gcn.GCN(n_channels)([output, input_graph])
-  pool, pool_assignment = dmon.DMoN(
-      FLAGS.n_clusters,
-      collapse_regularization=FLAGS.collapse_regularization,
-      dropout_rate=FLAGS.dropout_rate)([output, input_adjacency])
-  return tf.keras.Model(
-      inputs=[input_features, input_graph, input_adjacency],
-      outputs=[pool, pool_assignment])
+    Returns:
+        Built Keras DMoN model.
+    """
+    output = input_features
+    for n_channels in FLAGS.architecture:
+        output = gcn.GCN(n_channels)([output, input_graph])
+    pool, pool_assignment = dmon.DMoN(
+        FLAGS.n_clusters,
+        collapse_regularization=FLAGS.collapse_regularization,
+        dropout_rate=FLAGS.dropout_rate)([output, input_adjacency])
+    return tf.keras.Model(
+        inputs=[input_features, input_graph, input_adjacency],
+        outputs=[pool, pool_assignment])
 
 
 def main(argv):
-  if len(argv) > 1:
-    raise app.UsageError('Too many command-line arguments.')
-  # Load and process the data (convert node features to dense, normalize the
-  # graph, convert it to Tensorflow sparse tensor.
-  adjacency, features, labels, label_indices = load_npz(FLAGS.graph_path)
-  features = features.todense()
-  n_nodes = adjacency.shape[0]
-  feature_size = features.shape[1]
-  graph = convert_scipy_sparse_to_sparse_tensor(adjacency)
-  graph_normalized = convert_scipy_sparse_to_sparse_tensor(
-      utils.normalize_graph(adjacency.copy()))
+    if len(argv) > 1:
+        raise app.UsageError('Too many command-line arguments.')
+    # Load and process the data (convert node features to dense, normalize the
+    # graph, convert it to Tensorflow sparse tensor.
+    adjacency, features = load_npz(FLAGS.graph_path)
+    features = features.todense()
+    n_nodes = adjacency.shape[0]
+    feature_size = features.shape[1]
+    graph = convert_scipy_sparse_to_sparse_tensor(adjacency)
+    graph_normalized = convert_scipy_sparse_to_sparse_tensor(
+        utils.normalize_graph(adjacency.copy()))
 
-  # Create model input placeholders of appropriate size
-  input_features = tf.keras.layers.Input(shape=(feature_size,))
-  input_graph = tf.keras.layers.Input((n_nodes,), sparse=True)
-  input_adjacency = tf.keras.layers.Input((n_nodes,), sparse=True)
+    # Create model input placeholders of appropriate size
+    input_features = tf.keras.layers.Input(shape=(feature_size,))
+    input_graph = tf.keras.layers.Input((n_nodes,), sparse=True)
+    input_adjacency = tf.keras.layers.Input((n_nodes,), sparse=True)
 
-  model = build_dmon(input_features, input_graph, input_adjacency)
+    model = build_dmon(input_features, input_graph, input_adjacency)
 
-  # Computes the gradients wrt. the sum of losses, returns a list of them.
-  def grad(model, inputs):
-    with tf.GradientTape() as tape:
-      _ = model(inputs, training=True)
-      loss_value = sum(model.losses)
-    return model.losses, tape.gradient(loss_value, model.trainable_variables)
+    # Computes the gradients wrt. the sum of losses, returns a list of them.
+    def grad(model, inputs):
+        with tf.GradientTape() as tape:
+            _ = model(inputs, training=True)
+            loss_value = sum(model.losses)
+        return model.losses, tape.gradient(loss_value, model.trainable_variables)
 
-  optimizer = tf.keras.optimizers.Adam(FLAGS.learning_rate)
-  model.compile(optimizer, None)
+    optimizer = tf.keras.optimizers.Adam(FLAGS.learning_rate)
+    model.compile(optimizer, None)
 
-  for epoch in range(FLAGS.n_epochs):
-    loss_values, grads = grad(model, [features, graph_normalized, graph])
-    optimizer.apply_gradients(zip(grads, model.trainable_variables))
-    print(f'epoch {epoch}, losses: ' +
-          ' '.join([f'{loss_value.numpy():.4f}' for loss_value in loss_values]))
+    for epoch in range(FLAGS.n_epochs):
+        loss_values, grads = grad(model, [features, graph_normalized, graph])
+        optimizer.apply_gradients(zip(grads, model.trainable_variables))
+        print(f'epoch {epoch}, losses: ' +
+              ' '.join([f'{loss_value.numpy():.4f}' for loss_value in loss_values]))
 
-  # Obtain the cluster assignments.
-  _, assignments = model([features, graph_normalized, graph], training=False)
-  assignments = assignments.numpy()
-  clusters = assignments.argmax(axis=1)  # Convert soft to hard clusters.
+    # Obtain the cluster assignments.
+    _, assignments = model([features, graph_normalized, graph], training=False)
+    assignments = assignments.numpy()
+    clusters = assignments.argmax(axis=1)  # Convert soft to hard clusters.
 
-  # Prints some metrics used in the paper.
-  print('Conductance:', metrics.conductance(adjacency, clusters))
-  print('Modularity:', metrics.modularity(adjacency, clusters))
-  print(
-      'NMI:',
-      sklearn.metrics.normalized_mutual_info_score(
-          labels, clusters[label_indices], average_method='arithmetic'))
-  precision = metrics.pairwise_precision(labels, clusters[label_indices])
-  recall = metrics.pairwise_recall(labels, clusters[label_indices])
-  print('F1:', 2 * precision * recall / (precision + recall))
+    # Save cluster assignments
+    np.save('cluster_assignments.npy', clusters)
+    print("Cluster assignments saved to cluster_assignments.npy")
+
+    # Print cluster assignments
+    print("Cluster Assignments:")
+    for node, cluster in enumerate(clusters):
+        print(f"Node {node}: Cluster {cluster}")
 
 
 if __name__ == '__main__':
-  app.run(main)
+    app.run(main)
